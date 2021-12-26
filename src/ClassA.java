12faa3b9357a61f9bcd10027c88b21409459effb
Akash <aryanakashshelke@gmail.com>
2021-12-26 09:43:11 +0530

    First Commit

---
+++ src/ClassA.java
@@ -0,0 +1,7 @@
+public class ClassA {
+    public static void main(String[] args) {
+        System.out.println("Hello World! I am Java Junior");
+        System.out.println("this is the first code on github ");
+
+    }
+}
